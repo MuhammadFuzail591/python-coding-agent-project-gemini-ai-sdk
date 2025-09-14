fed26e3ab099c8885d97bbb53345efbefe1c8153
Muhammad Fuzail <muhammadfuzail@gmail.com>
2025-09-14 08:26:08 +0500

    done calculator problem and working on get_file_info function now.

---
+++ main.py
@@ -0,0 +1,43 @@
+import sys
+import os
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
+
+
+def main():
+
+    load_dotenv()
+
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+    verbose_flag = False
+
+    if len(sys.argv) < 2:
+        print("Please Give prompt")
+        sys.exit(1)
+    if len(sys.argv) == 3 and sys.argv[2] == "--verbose":
+        verbose_flag = True
+    
+    prompt = sys.argv[1]
+
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=prompt)])
+    ]
+    response = client.models.generate_content(
+           model="gemini-2.0-flash-lite",
+           contents=messages
+        )
+
+    print(response.text)
+
+    if verbose_flag:
+        print(f"User prompt: {prompt}")
+        if response and response.usage_metadata:
+            print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+            print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+    
+    sys.exit(0)
+
+main()
