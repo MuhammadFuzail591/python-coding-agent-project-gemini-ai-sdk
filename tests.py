901ab9159cb1c2568211faebd418683052de8ad7
Muhammad Fuzail <muhammadfuzail@gmail.com>
2025-09-15 02:07:36 +0500

    Done with write_file function for the llm.

--- tests.py
+++ tests.py
@@ -1,16 +1,15 @@
 
 from functions.get_file_content import get_file_content
-
+from functions.write_file import write_file
 def main():
 
-   main_res = get_file_content("calculator","main.py")
-   pkg_res = get_file_content("calculator","pkg/calculator.py")
-   bin_res = get_file_content("calculator","/bin/cat")
-   does_res = get_file_content("calculator","pkg/does_not_exist.py")
+   main_res = write_file("calculator","lorem.txt", "wait, this isn't lorem ipsum")
+   pkg_res = write_file("calculator","pkg/morelorem.txt", "lorem ipsum dolor sit amet")
+
+   does_res = write_file("calculator","tmp/temp.txt","this should not be allowed")
 
    print(main_res)
    print(pkg_res)
-   print(bin_res)
    print(does_res)
 
 main()
